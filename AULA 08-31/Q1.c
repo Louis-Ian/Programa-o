7dcbc811e6531e728b234b20ea6c5857260fc4bb
Louis-Ian <louisiangt@gmail.com>
2017-08-31 15:33:52 -0300

    Add files via upload

--- AULA 08-31/Q1.c
+++ AULA 08-31/Q1.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-	int diaNasc,mesNasc,anoNasc;
-	int diaHoje,mesHoje,anoHoje;
-	int idadeDia, idadeMes, idadeAno,iDADE;
+	int diaNasc,mesNasc,anoNasc,diaHoje,mesHoje,anoHoje;
+	int idadeAno,idade;
+	int teste = 666;
 	
 	printf("Insira, respectivamente, dia, mês e ano do nascimento:\n");
 	scanf("%d  %d  %d", &diaNasc, &mesNasc, &anoNasc);
@@ -12,33 +12,81 @@ int main(int argc, char **argv){
 	printf("Insira, respectivamente, dia, mês e ano de hoje:\n");
 	scanf("%d  %d %d", &diaHoje, &mesHoje, &anoHoje);
 
-	idadeAno = anoHoje - anoNasc;
-	idadeMes = mesHoje - mesNasc;
-	idadeDia = diaHoje - diaNasc;
+	idadeAno = anoHoje - anoNasc + 1;
 
-	while(anoNasc < anoHoje){
-		if (((anoHoje%4)==0) && ((anoHoje%400)==0) && ((anoHoje%100)!=0)){
-			idadeDia++;
-		}
-		anoHoje--;
-	}
+	idade = idadeAno*365;
 
-	if (idadeMes < 0){
-		idadeMes = 12 + idadeMes;
-		idadeAno--;
-	}
-
-	iDADE = 365*idadeAno;
-
-	for (int i = 0; i < idadeAno; ++i)
+	for(int i = anoNasc ; i < anoHoje ; ++i)
 	{
-		iDADE++;
+		if ((i%4)==0 && (i%400)==0 && (i%100)!=0)
+		{
+			idade++ ;
+		}
 	}
 
+	for (int i = 1; i < mesNasc; ++i)
+	{
+		if (i <= 7)
+		{
+			if (i % 2 == 0 && i != 2)
+			{
+				idade -= 30;
+			}
+			else if(i % 2 == 1)
+			{
+				idade -= 31;
+			}
+			else
+			{
+				idade -= 29;
+			}
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				idade -= 31;
+			}
+			else if(i % 2 == 1)
+			{
+				idade -= 30;
+			}
+		}
+	}
 
+	for (int i = mesHoje; i <= 12; ++i)
+	{
+		if (i <= 7)
+		{
+			if (i % 2 == 0 && i != 2)
+			{
+				idade -= 30;
+			}
+			else if(i % 2 == 1)
+			{
+				idade -= 31;
+			}
+			else
+			{
+				idade -= 29;
+			}
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				idade -= 31;
+			}
+			else if(i % 2 == 1)
+			{
+				idade -= 30;
+			}
+		}
+	}
 
-	//printf("Sua idade é: %d dias, %d meses e %d anos \n", idadeDia, idadeMes, idadeAno);
-	printf("Sua idade é: %d \n", iDADE);
+	idade += diaHoje;
+
+	printf("Sua idade é: %d \n", idade);
 
 	return 0;
 }
--- AULA 08-31/Q1_2.c
+++ AULA 08-31/Q1_2.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv){
 	int diaNasc,mesNasc,anoNasc;
 	int diaHoje,mesHoje,anoHoje;
-	int idadeDia, idadeMes, idadeAno,iDADE=0;
+	int idade=0, anos, dias;
 
 	printf("Insira, respectivamente, dia, mês e ano do nascimento:\n");
 	scanf("%d  %d  %d", &diaNasc, &mesNasc, &anoNasc);
@@ -12,31 +12,164 @@ int main(int argc, char **argv){
 	printf("Insira, respectivamente, dia, mês e ano de hoje:\n");
 	scanf("%d  %d %d", &diaHoje, &mesHoje, &anoHoje);
 
-	idadeAno = anoHoje - anoNasc;
-	idadeMes = mesHoje - mesNasc;
+	anos = anoHoje - anoNasc;
+	dias = diaHoje;
 
-	if (idadeMes < 0){
-		idadeMes = 12 + idadeMes;
-		idadeAno--;
+	if (mesNasc <= 7)
+		{
+			if (mesNasc % 2 == 0 && mesNasc != 2)
+			{
+				idade += 30;
+			}
+			else if(mesNasc % 2 == 1)
+			{
+				idade += 31;
+			}
+			else if(mesNasc == 2)
+			{
+				if (((anoNasc%4)==0) && ((anoNasc%400)==0) && ((anoNasc%100)!=0))
+				{
+					idade += 29;
+				}
+				else{
+					idade += 28;
+				}
+			}
+		}
+		else
+		{
+			if (diaNasc % 2 == 0)
+			{
+				idade += 31;
+			}
+			else if(diaNasc % 2 == 1)
+			{
+				idade += 30;
+			}
+		}
+
+	idade += diaNasc;
+
+	for (int i = mesNasc + 1; i <= 12; ++i)
+	{
+		if (i <= 7)
+		{
+			if (i % 2 == 0 && i != 2)
+			{
+				idade += 30;
+			}
+			else if(i % 2 == 1)
+			{
+				idade += 31;
+			}
+			else if(i == 2)
+			{
+				if (((anoNasc%4)==0) && ((anoNasc%400)==0) && ((anoNasc%100)!=0))
+				{
+					idade += 29;
+				}
+				else{
+					idade += 28;
+				}
+			}
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				idade += 31;
+			}
+			else if(i % 2 == 1)
+			{
+				idade += 30;
+			}
+		}
 	}
 
-	for (int i = 1; i < 12; ++i)
+	for (int i = 1; i < mesHoje; ++i)
 	{
-		
-
-		for (int i = 0; i < idadeAno; ++i)
+		if (i <= 7)
 		{
-			if (((anoHoje%4)==0) && ((anoHoje%400)==0) && ((anoHoje%100)!=0))
+			if (i % 2 == 0 && i != 2)
 			{
-				iDADE++;
-				anoHoje--;
+				idade += 30;
+			}
+			else if(i % 2 == 1)
+			{
+				idade += 31;
+			}
+			else if(i == 2)
+			{
+				if (((anoHoje%4)==0) && ((anoHoje%400)==0) && ((anoHoje%100)!=0))
+				{
+					idade += 29;
+				}
+				else
+				{
+					idade += 28;
+				}
+			}
+		}
+		else
+		{
+			if (i % 2 == 0)
+			{
+				idade += 31;
+			}
+			else if(i % 2 == 1)
+			{
+				idade += 30;
+			}
+		}
+	}
+
+	if (anos >1)
+	{
+		for (int j = anoNasc + 1; j < anoHoje ; ++j)
+		{
+			for (int i = 1; i <= 12; ++i)
+			{
+				if (i <= 7)
+				{
+					if (i % 2 == 0 && i != 2)
+					{
+						idade += 30;
+					}
+					else if(i % 2 == 1)
+					{
+						idade += 31;
+					}
+					else if(i == 2)
+					{
+						if (((anoNasc%4)==0) && ((anoNasc%400)==0) && ((anoNasc%100)!=0))
+						{
+							idade += 29;
+						}
+						else
+						{
+							idade += 28;
+						}
+					}
+				}
+				else
+				{
+					if (i % 2 == 0)
+					{
+						idade += 31;
+					}
+					else if(i % 2 == 1)
+					{
+						idade += 30;
+					}
+				}
 			}
 			
 		}
-		
 	}
 
-	printf("Sua idade é: %d \n", iDADE);
+
+
+	printf("Sua idade é: %d dias! \n", idade);
 
 	return 0;
 }
