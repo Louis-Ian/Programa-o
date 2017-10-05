4d820e2e103e1ac497912142015b0ca0a82d78cc
louis ian <louisiangt@gmail.com>
2017-10-05 17:18:03 -0300

    vetores transformados em ponteiros

--- AULA 05 - 10/TLAB-06.c
+++ AULA 05 - 10/TLAB-06.c
@@ -4,7 +4,7 @@
 int str_tamanho (char *s)
 {
 	int tam = 0;
-	while(s[tam] != '\0')
+	while(*(s+tam) != '\0')
 	{
 		tam++;
 	}
@@ -15,11 +15,13 @@ int str_tamanho (char *s)
 void str_copia (char *s, char *d)
 {
 	int i = 0;
-	while(s[i] != '\0')
+	//d = (char*)malloc(str_tamanho(s)*sizeof(char));
+	while(*(s+i) != '\0')
 	{
-		d[i] = s[i];
+		*(d+i) = *(s+i);
 		i++;
 	}
+	//printf("%s\n", d);
 }
 
 void str_concatena (char *s, char *d)
@@ -34,34 +36,44 @@ void str_concatena (char *s, char *d)
 
 	while(i<S)
 	{
- 		*(V+i) = s[i];
+ 		*(V+S+i) = *(s+i);
  		i++;
 	}
 	while(j<D)
 	{
-		*(V+S+j) = d[j];
+		*(V+j) = *(d+j);
 		j++;
 	}
+
+	str_copia(V,d);
 }
 
 int main(int argc, char const *argv[])
 {
-	char t1[5] = "12345";
-	printf("%s \n", t1);
+	char *t1 = (char*)malloc(5*sizeof(char));
+	t1 = "12345";
 
-	int tamanho1 = str_tamanho(t1);
-	printf("%d\n", tamanho1);
+	char *t2 = (char*)malloc(7*sizeof(char));
+	t2 = "6789011";
 
-	char t2[10] = "12345";
-	char t3[20] = "";
+	char *t3 = (char*)malloc(8*sizeof(char));
+	int tamanho1 = str_tamanho(t3);
+	printf("tam: %d\n", tamanho1);
+	printf("t1: %s\n", t1);
+	printf("t2: %s\n", t2);
+	printf("t3: %s\n\n", t3);
 
 	str_copia(t1,t3);
 
-	printf("%s\n", t3);
+	printf("t1: %s\n", t1);
+	printf("t2: %s\n", t2);
+	printf("t3: %s\n\n", t3);
 
 	str_concatena(t2,t3);
 
-	printf("%s\n", t3);
+	printf("t1: %s\n", t1);
+	printf("t2: %s\n", t2);
+	printf("t3: %s\n\n", t3);
 
 	return 0;
 }
