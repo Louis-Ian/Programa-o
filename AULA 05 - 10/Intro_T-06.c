4d02084361ec6526a9b6b4a00f81908b91f6b2bf
Louis-Ian <louisiangt@gmail.com>
2017-10-05 11:20:47 -0300

    Aula do dia 5 de OUT

---
+++ AULA 05 - 10/Intro_T-06.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+***	INTRODUÇÃO À STRING ***
+
+int main(int argc, char const *argv[])
+{
+	char a = 'a';
+	printf("%c\n", a);
+
+	char b[10] = "bbbbbbbbb";
+	printf("%s\n", b);
+
+	char *c = "num f0de coroi!";
+	int i = 0;
+	while(c[i] != '\0') // \0 = fim da string
+	{
+		printf("%c\n", c[i]);
+		i++;
+	}
+
+	char d[5];
+	scanf("%s\n", d);
+	printf("você escreveu: %s\n", d);
+
+	char *a1 = (char*)malloc(10*sizeof(char));
+	i = 0;
+	a1 = "letra letra";
+	while(a1[i] != '\0')
+	{
+		printf("%c\n", a1[i]);
+		i++;
+	}
+	printf("%d\n", i); // tamanho da string
+
+	return 0;
+}
+*/
---
+++ AULA 05 - 10/TLAB-06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int str_tamanho (char *s)
+{
+	int tam = 0;
+	while(s[i] != '\0')
+	{
+		tam++;
+	}
+
+	return tam;
+}
+
+void str_copia (char *s, char *d)
+{
+
+
+}
+
+void str_concatena (char *s, char *d)
+{
+
+
+}
+
+int main(int argc, char const *argv[1])
+{
+	
+
+	return 0;
+}
