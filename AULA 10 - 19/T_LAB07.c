6ce29bc897acffe730dbdcc7a870b7b82d326b23
ian santos <louisiangt@gmail.com>
2017-10-20 13:58:39 -0300

    o struct Info funciona. ERRO: if info.

--- AULA 10 - 19/T_LAB07.c
+++ AULA 10 - 19/T_LAB07.c
@@ -6,11 +6,11 @@
 #define MAXREGISTROS 1000
 
 typedef struct info{
-	char nome[100];
+	char nome[MAXNOME];
 	long int matricula;
 	long int numero;
 	int ddd;
-	char tipo[2];
+	char tipo[1];
 } Info;
 
 void buscar_nome(Info *agenda){
@@ -18,8 +18,7 @@ void buscar_nome(Info *agenda){
 }
 
 void inserir(Info *agenda){
-	Info insert;
-	printf("Insira o nome: \n");
+	/*printf("Insira o nome: \n");
 	scanf("%[^\n]", agenda->nome);
 	printf("Insira a matrícula: \n");
 	scanf("%ld", &agenda->matricula);
@@ -27,14 +26,13 @@ void inserir(Info *agenda){
 	scanf("%d", &agenda->ddd);
 	printf("Insira o número: \n");
 	scanf("%ld", &agenda->numero);
-	printf("Insira o tipo: \n");
+	*/printf("Insira o tipo: \n");
 	scanf("%s", agenda->tipo);
 }
 
-char menu(Info *agenda){
-	if (agenda->tipo == "A")
-	{
-		return "A";
+void menu(Info *agenda){
+	if (agenda->tipo == 'A'){
+		printf("A...........\n");
 	}
 }
 
@@ -42,7 +40,7 @@ int main(int argc, char const *argv[]){
 	
 	Info teste;
 	inserir(&teste);
-	char vamos[1];
-	vamos = menu(&teste);
+	menu(&teste);
+
 	return 0;
 }
